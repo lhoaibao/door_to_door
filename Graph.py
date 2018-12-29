942f4a5113cde4d7c8295cda4cb1faea30d44f85
lhoaibao <lehoaibao081999@gmail.com>
2018-12-29 23:40:39 +0700

    ne

--- Graph.py
+++ Graph.py
@@ -1,3 +1,7 @@
+from Node import Node
+import os
+
+
 class Graph:
     """
     Graph class has 3 attribute:
@@ -6,27 +10,45 @@ class Graph:
         - length is distance from the start to the end get by result
     *
     """
-    def __init__(self, list_city):
-        """
-        """
-        self.list_city = list_city
+    def __init__(self):
+        self.list_city = []
         self.result = []
         self.length = 0
 
+    def distance(self, n1, n2):
+        return ((n1.latitude-n2.latitude)**2+(n1.longitude-n2.longitude)**2)**0.5
+
+    def load_graph(self, file_name):
+        """
+        function read file csv and get content of file into
+        a list
+        -   return file_content when file exists and can read
+        -   return None when file cant read or doesnt exists
+        """
+        list_city = []
+        if os.access(file_name,os.F_OK):
+            if os.access(file_name,os.R_OK):
+                with open(file_name,'r') as f:
+                    file_content = f.readlines()
+                for i in range(len(file_content)):
+                    x = file_content[i].strip('\n').split(', ')
+                    list_city.append(Node(x[0],float(x[1]),float(x[2])))
+                self.list_city = list_city
+                return True
+        return False
+
     def nearest_neighbor(self):
-        """
-        """
         sum = 0
-        check_list = self.list_city.copy()
-        first = self.list_city[0]
         result = []
+        check_list = self.list_city
+        first = self.list_city[0]
         result.append(first)
         check_list.remove(first)
         while check_list:
-            min = ((result[-1].latitude-check_list[0].latitude)**2+(result[-1].longitude-check_list[0].longitude)**2)**0.5
+            min = self.distance(result[-1], check_list[0])
             nearest = check_list[0]
             for i in range(1,len(check_list)):
-                check = ((result[-1].latitude-check_list[i].latitude)**2+(result[-1].longitude-check_list[i].longitude)**2)**0.5
+                check = self.distance(result[-1], check_list[i])
                 if check < min:
                     min = check
                     nearest = check_list[i]
@@ -37,10 +59,8 @@ class Graph:
         self.result = result
 
     def print_result(self):
-        """
-        """
         result = self.result
         for i in range(len(result)-1):
-            print(result[i].city_name, end='-> ')
+            print(result[i].city_name, end= ' -> ')
         print(result[-1].city_name)
         print('length: ', self.length)
--- tsp.py
+++ tsp.py
@@ -1,68 +1,30 @@
 #!/usr/bin/env python3
 import sys
-import os
-from Node import Node
-from Graph import Graph
 import time
-from two_opt import two_opt
-
-
-def read_file(file_name):
-    """
-    function read file csv and get content of file into
-    a list
-    -   return file_content when file exists and can read
-    -   return None when file cant read or doesnt exists
-    """
-    file_content = []
-    if os.access(file_name,os.F_OK):
-        if os.access(file_name,os.R_OK):
-            with open(file_name,'r') as f:
-                file_content = f.readlines()
-            for i in range(len(file_content)):
-                file_content[i] = file_content[i].strip('\n').split(', ')
-                file_content[i][1] = float(file_content[i][1])
-                file_content[i][2] = float(file_content[i][2])
-            return file_content
-    return None
-
-
-def parse_map(cities):
-    """
-    function parse a list of city in to a object Graph is a dictionary
-    key of dic is city_name
-    value is a object Node has two arg is latitude and longitude
-    -   return a Graph object if list of city not None
-    -   return None if list of city None
-    """
-    list_city = []
-    for i in range(len(cities)):
-        list_city.append(Node(cities[i][0], cities[i][1], cities[i][2]))
-    return Graph(list_city)
+from Graph import Graph
 
 
 def main():
-    if len(sys.argv)<=1:
-        print('Wrong command')
-        return False
-    file_content = read_file(sys.argv[1])
-    if not file_content:
-        print('there are something wrong with input file')
-        return False
-    graph = parse_map(file_content)
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
+        print('wrong command')
+        return None
+    graph = Graph()
+    load = graph.load_graph(sys.argv[1])
+    if not load:
+        print('Something wrong with file')
+        return None
     if len(sys.argv) == 2:
         start = time.time()
         result = graph.nearest_neighbor()
-        end = time.time()
-        graph.print_result()
-        print(end-start)
-        return True
-    elif len(sys.argv) == 3:
-        pass
+    elif sys.argv[2] == 'nearest_neighbor':
+        start = time.time()
+        result = graph.nearest_neighbor()
     else:
-        print('Wrong command')
-        return False
+        print('algo is not found')
+        return None
+    end = time.time()
+    print('time run: ',end-start)
 
 
 if __name__ == '__main__':
-    process = main()
+    main()
